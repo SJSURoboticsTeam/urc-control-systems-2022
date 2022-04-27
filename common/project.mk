053e91f39fbeefeed77bbc6f8a318ea58781a3d8
Nathanael Garza <nate.garza@hotmail.com>
2022-04-26 22:49:34 -0700

    updates readme w/ important links

--- common/project.mk
+++
@@ -1,2 +0,0 @@
-TESTS += test/rover_system_test.cpp
-TESTS += test/heartbeat_test.cpp
--- subsystem-arm/project.mk
+++
@@ -1,8 +0,0 @@
-TESTS += test/joint_test.cpp
-TESTS += test/hand/finger_test.cpp
-TESTS += test/hand/hand_test.cpp
-TESTS += test/hand/pca9685_test.cpp
-TESTS += test/hand/wrist_joint_test.cpp
-TESTS += test/arm/arm_joint_test.cpp
-TESTS += test/arm/arm_test.cpp
-TESTS += test/arm_system_test.cpp
