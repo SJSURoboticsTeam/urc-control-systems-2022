f22a3bebb8ee7f2606ac79895da3e870afce9c66
naterpotatoers <nate.garze@hotmail.com>
2022-05-13 01:13:57 -0700

    adds serial command entry, working CI, refactored drive dto, and minimal esp driver

--- subsystem-drive/dto/drive-dto.hpp
+++ subsystem-drive/dto/drive-dto.hpp
@@ -1,27 +1,32 @@
 #pragma once
+
 namespace sjsu::drive
 {
     // note: postfix commands and arguments can be seperated into different files
     struct drive_commands
     {
-        int angle = 0; // -180 <--> 180
-        int speed = 0; // -100 <--> 100
-        int wheel_orientation = 0;
         char mode = 'D';
+        int speed = 0; // -100 <--> 100
+        int angle = 0; // -180 <--> 180
+        int wheel_orientation = 0;
+
+        void Print()
+        {
+            printf("Cmds\tMode\tSpeed\tAngle\tOri\n");
+            printf("\t%c\t%d\t%d\t%d\n", mode, speed, angle, wheel_orientation);
+        }
+    };
+
+    struct motor_arguments
+    {
+        float speed = 0;
+        float angle = 0;
     };
 
     struct leg_arguments
     {
-        struct motor_arguments
-        {
-            motor_arguments(float speed = 0, float angle = 0) : speed(speed), angle(angle)
-            {
-            }
-            float speed;
-            float angle;
-        };
-        motor_arguments steer{5}; //setting hub speed to a constant 5
-        motor_arguments hub{};
+        motor_arguments steer{motor_arguments{5, 0}};
+        motor_arguments hub;
     };
 
     struct tri_wheel_router_arguments
@@ -29,5 +34,13 @@ namespace sjsu::drive
         leg_arguments left{};
         leg_arguments right{};
         leg_arguments back{};
+
+        void Print()
+        {
+            printf("Args\tLeg\tSpeed\tAngle\n");
+            printf("\tLeft\t%.1f\t%.1f\n", static_cast<double>(left.hub.speed), static_cast<double>(left.steer.angle));
+            printf("\tRight\t%.1f\t%.1f\n", static_cast<double>(right.hub.speed), static_cast<double>(right.steer.angle));
+            printf("\tBack\t%.1f\t%.1f\n", static_cast<double>(back.hub.speed), static_cast<double>(back.steer.angle));
+        }
     };
 } // sjsu::drive
