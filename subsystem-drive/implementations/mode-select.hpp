3d80bc3f8cfe6d846a752c7e01aca3d1d8c74321
acarrou <dridriswim@gmail.com>
2022-05-13 17:27:01 -0700

    adds switch and select modes

--- subsystem-drive/implementations/mode-select.hpp
+++ subsystem-drive/implementations/mode-select.hpp
@@ -1,12 +1,28 @@
 #include "../dto/drive-dto.hpp"
+#include "../steer-modes.hpp"
 
 namespace sjsu::drive
 {
     class ModeSelect
     {
     public:
-        drive_commands SelectMode(drive_commands commands)
+        tri_wheel_router_arguments SelectMode(drive_commands commands)
         {
+            switch (commands.mode)
+            {
+            case 'D':
+                return SteerModes::DriveSteering(commands);
+                break;
+            case 'S':
+                return SteerModes::SpinSteering(commands);
+                break;
+            case 'T':
+                return SteerModes::TurnSteering(commands);
+                break;
+            default:
+                return tri_wheel_router_arguments{};
+                break;
+            }
         }
 
     private:
--- subsystem-drive/implementations/mode-switcher.hpp
+++ subsystem-drive/implementations/mode-switcher.hpp
@@ -1,4 +1,5 @@
 #include "../dto/drive-dto.hpp"
+#include "../steer-modes.hpp"
 
 namespace sjsu::drive
 {
@@ -6,19 +7,21 @@ namespace sjsu::drive
     {
     public:
         // Should handle all the logic for switching to new steer mode
-        drive_commands SwitchSteerMode(drive_commands commands)
+        drive_commands SwitchSteerMode(drive_commands commands, bool stopped)
         {
-            switch (commands.mode)
+            if (current_mode != commands.mode)
             {
-            case /* constant-expression */:
-                /* code */
-                break;
-
-            default:
-                break;
+                // Set all wheel speed equal to 0
+                commands.speed = 0;
+                commands.angle = 0;
+                commands.wheel_orientation = 0;
+                // Set rover to home position using home function
             }
+            current_mode = commands.mode;
+            return commands;
         }
 
     private:
+        char current_mode = 'D';
     };
 }
