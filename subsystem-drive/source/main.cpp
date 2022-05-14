0bce5219e2c66b457fed95a840cc676a23e7af5b
coreyboy1820 <coreyboy1820@hotmail.com>
2022-05-13 17:35:11 -0700

    editing main

--- subsystem-drive/source/main.cpp
+++ subsystem-drive/source/main.cpp
@@ -1,6 +1,10 @@
 #include "utility/log.hpp"
 
 #include "../implementations/steer-modes.hpp"
+#include "../implementations/tri-wheel-router.hpp"
+#include "../implementations/mode-switcher.hpp"
+#include "../implementations/mode-select.hpp"
+#include "../implementations/command-lerper.hpp"
 
 using namespace sjsu::drive;
 
@@ -15,14 +19,21 @@ drive_commands SerialEnterCommands()
 int main()
 {
     drive_commands commands;
+    tri_wheel_router_arguments arguments;
     SteerModes steermode;
+    TriWheelRouter tri_wheel;
+    CommandLerper lerp;
+    bool stopped = false;
     while (1)
     {
         commands = SerialEnterCommands();
         commands.Print();
         sjsu::Delay(2s);
 
-        tri_wheel_router_arguments arguments = steermode.DriveSteering(commands);
+        if(arguments.back.hub.speed == 0 && arguments.right.hub.speed == 0 && arguments.left.hub.speed == 0) stopped = true;
+        else stopped = false;
+        
+        arguments = tri_wheel.SetLegArguments(ModeSelect::SelectMode(lerp.Lerp(ModeSwitch::SwitchSteerMode(commands, stopped))));
         arguments.Print();
         sjsu::Delay(2s);
     }
