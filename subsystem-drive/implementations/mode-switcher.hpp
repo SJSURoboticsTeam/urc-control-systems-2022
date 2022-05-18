fbe6b087c50282e7f835bbf8a2fdb09117167e1e
coreyboy1820 <coreyboy1820@hotmail.com>
2022-05-17 22:51:04 -0700

    corrected mode switcher

--- subsystem-drive/implementations/mode-switcher.hpp
+++ subsystem-drive/implementations/mode-switcher.hpp
@@ -12,33 +12,36 @@ namespace sjsu::drive
         {
             bool hubs_stopped(previous_arguments.left.hub.speed == 0 && previous_arguments.right.hub.speed == 0 && previous_arguments.back.hub.speed == 0);
             bool steers_stopped(current_motor_feedback.left_steer_speed == 0_rpm && current_motor_feedback.right_steer_speed == 0_rpm && current_motor_feedback.back_steer_speed == 0_rpm);
-            if (previous_mode_ != commands.mode || switching_modes)
+            if (previous_mode_ != commands.mode)
             {
-                // Set all wheel speed equal to 0
+                in_the_middle_of_switchingmodes_ = true;
+                skip_once_ = true;
+            }
+            if (in_the_middle_of_switchingmodes_)
+            {
                 commands.speed = 0;
-                commands.angle = 0;
-                commands.wheel_orientation = 0;
-                // Set rover to home position using home function
-            }
-            if (hubs_stopped)
-            {
-                //if hubs are stopped and steers are not stopped then we are switching modes, but if hubs are stopped and steers are stopped, then we are not switching modes
-                !steers_stopped ? switching_modes = true : switching_modes = false;
-            }
-            if (!hubs_stopped && previous_mode_ != commands.mode)
-            {
-                commands.mode = previous_mode_;
-            }
-            else
-            {
+                if (!hubs_stopped)
+                {
+                    commands.mode = previous_mode_;
+                    return commands;
+                } // hubs must be stopped to pass here
+                else if (skip_once_ && steers_stopped)
+                {
+                    skip_once_ = false;
+                } // will start moving steer motors before exiting middle of switching modes
+                else if (steers_stopped)
+                {
+                    in_the_middle_of_switchingmodes_ = false;
+                    skip_once_ = true;
+                } // only once steer motors have stopped moving after hubs stopped will we exit switching modes
                 previous_mode_ = commands.mode;
             }
-
             return commands;
         }
 
     private:
-        char previous_mode_ = 'D'; // set to O to represent off in the beginning
-        bool switching_modes = true;
+        char previous_mode_ = 'D';
+        bool in_the_middle_of_switchingmodes_ = true;
+        bool skip_once_ = true;
     };
 }
