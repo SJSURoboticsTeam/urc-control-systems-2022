a07ffde4ff785abe9a663de6be0765e9d45c1842
Gene Luong <luonggene@gmail.com>
2022-05-28 12:42:14 -0700

    router:SetArmArguements and modes:SimultaneousMode added

--- subsystem-arm/implementations/arm-modes.hpp
+++ subsystem-arm/implementations/arm-modes.hpp
@@ -9,10 +9,30 @@ namespace sjsus::arm
         public:
 
         static arm_arguments SimulataneousMode(arm_commands commands)
-        {}
+        {
+            arm_arguments temp;
+            temp.rotunda_angle = anglesconvert(commands.rotunda_angle);
+            temp.shoulder_angle = anglesconvert(commands.shoulder_angle);
+            temp.elbow_angle = anglesconvert(commands.elbow_angle);
+            temp.wrist_pitch_angle = anglesconvert(commands.wrist_pitch_angle);
+            temp.wrist_yaw_angle = anglesconvert(commands.wrist_yaw_angle);
+            temp.finger_angle = anglesconvert(commands.finger_angle);
+            return{}
+        }
 
         static arm_arguments CondensedMode(arm_commands commands)
-        {}
+        {
+
+        }
         //TODO: come up with other possible modes
+        private:
+        int anglesconvert(int a){
+            int temp;
+            temp = a%360;
+            if(temp>180){
+                temp=-360;
+            }
+            return temp;
+        }
     };
 }
--- subsystem-arm/implementations/arm-router.hpp
+++ subsystem-arm/implementations/arm-router.hpp
@@ -15,16 +15,25 @@ namespace sjsu::arm
         {
         }
 
-        void Initialize(){};
+        void Initialize()
+        {
+            
+        };
 
         arm_arguments SetArmArguments(arm_arguments arguments)
         {
-            // TODO: Set angles for each motor
-            return {};
+
+            rotunda_.SetAngle(units::angle::degree_t(arguments.rotunda_angle));
+            shoulder_.SetAngle(units::angle::degree_t(arguments.shoulder_angle));
+            elbow_.SetAngle(units::angle::degree_t(arguments.elbow_angle));
+            left_wrist_.SetAngle(units::angle::degree_t(arguments.wrist_pitch_angle));//dunno if it's pitch or yaw fix when we test
+            right_wrist_.SetAngle(units::angle::degree_t(arguments.wrist_yaw_angle));
+            return {arguments};
         }
 
         void HomeArm(imu_accelerometers_feedback mpu_feedback, motors_feedback motors_feedback)
         {
+            
         }
 
     private:
