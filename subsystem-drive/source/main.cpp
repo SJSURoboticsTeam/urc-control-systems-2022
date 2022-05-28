b81bafe883423f93fdeb465041cca012e7100f49
acarrou <dridriswim@gmail.com>
2022-05-27 23:09:00 -0700

    uart coms

--- subsystem-drive/source/main.cpp
+++ subsystem-drive/source/main.cpp
@@ -1,5 +1,11 @@
 
 
+#include <cstdio>
+#include <iterator>
+
+#include "peripherals/lpc40xx/uart.hpp"
+#include "utility/log.hpp"
+#include "utility/time/time.hpp"
 #include "utility/log.hpp"
 
 const char response_body_format[] =
@@ -18,9 +24,27 @@ int main()
 {
     sjsu::LogInfo("STDIN Application Starting...\n");
 
-    int number = 0;
-    printf("Type in a number: ");
-    scanf("%s", response);
+    sjsu::lpc40xx::Uart &uart2 = sjsu::lpc40xx::GetUart<2>();
+
+    sjsu::LogInfo("Setting Baud rate to 38400");
+    uart2.settings.baud_rate = 38400;
+    uart2.Initialize();
+
+    std::array<uint8_t, 32> receive_buffer;
+
+    // Fill buffer with zeros
+    receive_buffer.fill(0);
+
+    // Read bytes into buffer until either the buffer is full or the UART
+    // peripheral no longer has any more bytes left.
+    // Store bytes into the kReadBytes variable.
+    const size_t kReadBytes = uart2.Read(receive_buffer);
+
+    // Print out the number of bytes read and the actual buffer
+    sjsu::LogInfo("(%zu) Bytes received: %.*s",
+                  kReadBytes,
+                  kReadBytes,
+                  receive_buffer.data());
 
     sscanf(
         response, response_body_format,
