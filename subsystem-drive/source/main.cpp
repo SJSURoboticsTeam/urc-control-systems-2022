9da0d3024249186507bd41e9861058a2df63557f
acarrou <dridriswim@gmail.com>
2022-05-27 22:28:43 -0700

    new test

--- subsystem-drive/source/main.cpp
+++ subsystem-drive/source/main.cpp
@@ -2,14 +2,30 @@
 
 #include "utility/log.hpp"
 
+const char response_body_format[] =
+    "\r\n\r\n{\n"
+    "  \"drive_mode\": \"%c\",\n"
+    "  \"speed\": %d,\n"
+    "  \"angle\": %d\n"
+    "  \"wheel_orientation\": %d,\n"
+    "}";
+
+char mode;
+int speed, angle, wheel_orientation;
+
 int main()
 {
-  sjsu::LogInfo("STDIN Application Starting...\n");
+    sjsu::LogInfo("STDIN Application Starting...\n");
 
-  int number = 0;
-  printf("Type in a number: ");
-  scanf("%d", &number);
-  printf("\n--> You typed number: %d\n\n", number);
+    int number = 0;
+    printf("Type in a number: ");
 
-  return 0;
+    sscanf(
+        response_body_format,
+        &mode, &speed, &angle,
+        &wheel_orientation);
+
+    printf("\n--> Mode %c\n \n--> Speed %d\n \n--> Angle %d\n \n--> Wheel_orientation %d\n", mode, speed, angle, wheel_orientation);
+
+    return 0;
 }
