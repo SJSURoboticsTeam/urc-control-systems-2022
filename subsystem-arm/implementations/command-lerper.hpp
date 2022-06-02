dad6c7fdec4051cb29a3d1a25a9cf260a2e9ffd8
Gene Luong <luonggene@gmail.com>
2022-06-01 20:36:45 -0700

    impliment lerp

--- subsystem-arm/implementations/command-lerper.hpp
+++ subsystem-arm/implementations/command-lerper.hpp
@@ -11,7 +11,15 @@ namespace sjsu::arm
             // TODO
             
             last_lerped_command_.elbow_angle = std::lerp(last_lerped_command_.elbow_angle, commands.elbow_angle, kSpeedLerp);
-            commands = last_lerped_command_;
+            last_lerped_command_.rotunda_angle = std::lerp(last_lerped_command_.rotunda_angle, commands.rotunda_angle, kSpeedLerp);
+            last_lerped_command_.shoulder_angle = std::lerp(last_lerped_command_.shoulder_angle, commands.shoulder_angle, kSpeedLerp);
+            last_lerped_command_.wrist_yaw_angle = std::lerp(last_lerped_command_.wrist_yaw_angle, commands.wrist_yaw_angle, kSpeedLerp);
+            last_lerped_command_.wrist_pitch_angle = std::lerp(last_lerped_command_.wrist_pitch_angle, commands.wrist_pitch_angle, kSpeedLerp);
+            commands.elbow_angle = last_lerped_command_.elbow_angle;
+            commands.rotunda_angle = last_lerped_command_.rotunda_angle;
+            commands.shoulder_angle = last_lerped_command_.shoulder_angle;
+            commands.wrist_pitch_angle = last_lerped_command_.wrist_pitch_angle;
+            commands.wrist_yaw_angle = last_lerped_command_.wrist_yaw_angle;
             return commands;
         }
 
