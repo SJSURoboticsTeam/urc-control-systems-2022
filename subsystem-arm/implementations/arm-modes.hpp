e107cb4e72e1487e437f6ec2b15a70a5c244ef00
mchan2003 <mchan100work@gmail.com>
2022-06-03 14:35:46 -0700

    added logic for CondensedMode

--- subsystem-arm/implementations/arm-modes.hpp
+++ subsystem-arm/implementations/arm-modes.hpp
@@ -2,18 +2,34 @@
 
 #include "../dto/arm-dto.hpp"
 
-namespace sjsus::arm
+namespace sjsu::arm
 {
     class ArmModes
     {
         public:
+            static constexpr float kCondRotundaAngle = 0;
+            static constexpr float kCondShoulderAngle = -18;
+            static constexpr float kCondElbowAngle = 74;
+            static constexpr float kCondWristPitchAngle = 0;
+            static constexpr float kCondWristYawAngle = 0;
+            static constexpr float kCondFingerAngle = 0;
+
             static arm_arguments SimulataneousMode(arm_commands commands)
             {}
 
             static arm_arguments CondensedMode(arm_commands commands)
             {
-                
+                arm_arguments temp;
+                temp.rotunda_angle = kCondRotundaAngle;
+                temp.shoulder_angle = kCondShoulderAngle;
+                temp.elbow_angle = kCondElbowAngle;
+                temp.wrist_pitch_angle = kCondWristPitchAngle;
+                temp.wrist_yaw_angle = kCondWristYawAngle;
+
+                return temp;
             }
+
+            
             //TODO: come up with other possible modes
     };
 }
--- subsystem-arm/test/arm-modes_test.cpp
+++ subsystem-arm/test/arm-modes_test.cpp
@@ -10,12 +10,12 @@ namespace sjsu::arm
         SECTION("should set angles to correct simulataneous mode angles")
         {
             arm_commands original_commands {100, 100, 100, 100, 100};
-            arm_arguments args = ArmModes::CondensedMode(original_commands);
+            arm_arguments args = ArmModes::SimulatiousMode(original_commands);
             CHECK(args.rotunda_angle == original_commands.rotunda_angle);
             CHECK(args.shoulder_angle == original_commands.shoulder_angle);
             CHECK(args.elbow_angle == original_commands.elbow_angle);
             CHECK(args.wrist_pitch_angle == original_commands.wrist_pitch_angle);
-            CHECK(args.wrist_pitch_angle == original_commands.wrist_pitch_angle);
+            CHECK(args.wrist_yaw_angle == original_commands.wrist_pitch_angle);
         }
 
         SECTION("should ignore arguments and set angles to proper condensed mode angles")
@@ -26,7 +26,7 @@ namespace sjsu::arm
             CHECK(args.shoulder_angle != original_commands.shoulder_angle);
             CHECK(args.elbow_angle != original_commands.elbow_angle);
             CHECK(args.wrist_pitch_angle != original_commands.wrist_pitch_angle);
-            CHECK(args.wrist_pitch_angle != original_commands.wrist_pitch_angle);
+            CHECK(args.wrist_yaw_angle != original_commands.wrist_pitch_angle);
         }
     }
 }
