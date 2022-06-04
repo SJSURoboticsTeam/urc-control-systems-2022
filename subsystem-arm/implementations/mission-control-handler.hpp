453bcd39ad64f98653113f40a538ee96f5580c24
Gene Luong <luonggene@gmail.com>
2022-06-04 13:02:03 -0700

    forgot to save

--- subsystem-arm/implementations/mission-control-handler.hpp
+++ subsystem-arm/implementations/mission-control-handler.hpp
@@ -20,22 +20,7 @@ namespace sjsu::arm
         }
         std::string CreateGETRequestParameterWithRoverStatus()
         {
-            char request_parameter[300];
-            snprintf(request_parameter, 300,
-                 "?heartbeat_count=%d&is_operational=%d&arm_mode=%c&hand_mode=%c&"
-                 "arm_speed=%d&battery=%d&rotunda_angle=%d&shoulder_angle=%d&elbow_"
-                 "angle=%d&wrist_roll=%d&wrist_pitch=%d&pinky_angle=%d&ring_angle=%"
-                 "d&middle_angle=%d&pointer_angle=%d&thumb_angle=%d",
-                 GetHeartbeatCount(), mc_data_.is_operational,
-                 static_cast<char>(arm_mc_data_.arm_mode),
-                 static_cast<char>(hand_mc_data_.hand_mode), int(mc_data_.speed),
-                 static_cast<int>(st.GetStateOfCharge()), arm_.GetRotundaPosition(),
-                 arm_.GetShoulderPosition(), arm_.GetElbowPosition(),
-                 hand_.GetWristRoll(), hand_.GetWristPitch(),
-                 hand_.GetPinkyPosition(), hand_.GetRingPosition(),
-                 hand_.GetMiddlePosition(), hand_.GetPointerPosition(),
-                 hand_.GetThumbPosition());
-            return request_parameter;
+            
         }
 
     private:
