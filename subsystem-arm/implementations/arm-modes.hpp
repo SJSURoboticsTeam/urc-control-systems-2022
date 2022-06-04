507c2e52de5ebc8ec7fb60ee5cb51539fa7fea86
FluffyFTW <46359603+FluffyFTW@users.noreply.github.com>
2022-06-04 12:05:02 -0700

    added added the SetArmArguements on router and put in SimultaneousMode for the modes (#41)
    * router:SetArmArguements and modes:SimultaneousMode added
    
    * fixed stylizations

--- subsystem-arm/implementations/arm-modes.hpp
+++ subsystem-arm/implementations/arm-modes.hpp
@@ -9,10 +9,30 @@ namespace sjsus::arm
         public:
 
         static arm_arguments SimulataneousMode(arm_commands commands)
-        {}
+        {
+            arm_arguments temp;
+            temp.rotunda_angle = AnglesConvert(commands.rotunda_angle);
+            temp.shoulder_angle = AnglesConvert(commands.shoulder_angle);
+            temp.elbow_angle = AnglesConvert(commands.elbow_angle);
+            temp.wrist_pitch_angle = AnglesConvert(commands.wrist_pitch_angle);
+            temp.wrist_yaw_angle = AnglesConvert(commands.wrist_yaw_angle);
+            temp.finger_angle = AnglesConvert(commands.finger_angle);
+            return temp;
+        }
 
         static arm_arguments CondensedMode(arm_commands commands)
-        {}
+        {
+
+        }
         //TODO: come up with other possible modes
+        private:
+        int AnglesConvert(int angle){
+            int temp;
+            temp = angle %360;
+            if(temp>180){
+                temp=-360;
+            }
+            return temp;
+        }
     };
 }
