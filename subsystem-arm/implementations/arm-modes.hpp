8c84c19c148983bdff81214daad95bca8a150937
Nathanael Garza <41933236+naterpotatoers@users.noreply.github.com>
2022-06-04 12:30:57 -0700

    arm lerp and mode select implementation (#42)
    * adds failing tests for lerping
    
    * Added some logic
    
    * impliment lerp
    
    * add arm mode tests
    
    * adds file to test arm
    
    * added logic for CondensedMode
    
    * renames Cond -> Condensed
    
    * remove finger angle and remove ConverAngle() for std::clamp()
    
    Co-authored-by: Andrew-Ingerman <andrewing777@gmail.com>
    Co-authored-by: Gene Luong <luonggene@gmail.com>
    Co-authored-by: mchan2003 <mchan100work@gmail.com>

--- subsystem-arm/implementations/arm-modes.hpp
+++ subsystem-arm/implementations/arm-modes.hpp
@@ -2,37 +2,41 @@
 
 #include "../dto/arm-dto.hpp"
 
-namespace sjsus::arm
+namespace sjsu::arm
 {
     class ArmModes
     {
         public:
-
+            static constexpr float kCondensedRotundaAngle = 0;
+            static constexpr float kCondensedShoulderAngle = -18;
+            static constexpr float kCondensedElbowAngle = 74;
+            static constexpr float kCondensedWristPitchAngle = 0;
+            static constexpr float kCondensedWristYawAngle = 0;
+            static constexpr float kCondensedFingerAngle = 0;
+            
+        //TODO: come up with other possible modes
         static arm_arguments SimulataneousMode(arm_commands commands)
         {
             arm_arguments temp;
-            temp.rotunda_angle = AnglesConvert(commands.rotunda_angle);
-            temp.shoulder_angle = AnglesConvert(commands.shoulder_angle);
-            temp.elbow_angle = AnglesConvert(commands.elbow_angle);
-            temp.wrist_pitch_angle = AnglesConvert(commands.wrist_pitch_angle);
-            temp.wrist_yaw_angle = AnglesConvert(commands.wrist_yaw_angle);
-            temp.finger_angle = AnglesConvert(commands.finger_angle);
+            temp.rotunda_angle = static_cast<int>(std::clamp(commands.rotunda_angle, -180, 180));
+            temp.shoulder_angle = static_cast<int>(std::clamp(commands.shoulder_angle, -180, 180));
+            temp.elbow_angle = static_cast<int>(std::clamp(commands.elbow_angle, -180, 180));
+            temp.wrist_pitch_angle = static_cast<int>(std::clamp(commands.wrist_pitch_angle, -180, 180));
+            temp.wrist_yaw_angle = static_cast<int>(std::clamp(commands.wrist_yaw_angle, -180, 180));
             return temp;
         }
 
         static arm_arguments CondensedMode(arm_commands commands)
         {
+            arm_arguments temp;
+            temp.rotunda_angle = kCondensedRotundaAngle;
+            temp.shoulder_angle = kCondensedShoulderAngle;
+            temp.elbow_angle = kCondensedElbowAngle;
+            temp.wrist_pitch_angle = kCondensedWristPitchAngle;
+            temp.wrist_yaw_angle = kCondensedWristYawAngle;
 
-        }
-        //TODO: come up with other possible modes
-        private:
-        int AnglesConvert(int angle){
-            int temp;
-            temp = angle %360;
-            if(temp>180){
-                temp=-360;
-            }
             return temp;
         }
+        //TODO: come up with other possible modes
     };
 }
--- subsystem-arm/implementations/command-lerper.hpp
+++ subsystem-arm/implementations/command-lerper.hpp
@@ -8,7 +8,16 @@ namespace sjsu::arm
     public:
         arm_commands Lerp(arm_commands commands)
         {
-            //TODO
+            last_lerped_command_.elbow_angle = std::lerp(last_lerped_command_.elbow_angle, commands.elbow_angle, kSpeedLerp);
+            last_lerped_command_.rotunda_angle = std::lerp(last_lerped_command_.rotunda_angle, commands.rotunda_angle, kSpeedLerp);
+            last_lerped_command_.shoulder_angle = std::lerp(last_lerped_command_.shoulder_angle, commands.shoulder_angle, kSpeedLerp);
+            last_lerped_command_.wrist_yaw_angle = std::lerp(last_lerped_command_.wrist_yaw_angle, commands.wrist_yaw_angle, kSpeedLerp);
+            last_lerped_command_.wrist_pitch_angle = std::lerp(last_lerped_command_.wrist_pitch_angle, commands.wrist_pitch_angle, kSpeedLerp);
+            commands.elbow_angle = last_lerped_command_.elbow_angle;
+            commands.rotunda_angle = last_lerped_command_.rotunda_angle;
+            commands.shoulder_angle = last_lerped_command_.shoulder_angle;
+            commands.wrist_pitch_angle = last_lerped_command_.wrist_pitch_angle;
+            commands.wrist_yaw_angle = last_lerped_command_.wrist_yaw_angle;
             return commands;
         }
 
