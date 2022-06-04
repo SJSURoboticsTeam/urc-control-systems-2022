df8820af41b6391f1be67665fe7bd5f623c50806
mchan2003 <mchan100work@gmail.com>
2022-06-04 14:12:13 -0700

    Implemented logic for mpu router

--- subsystem-arm/implementations/mpu-router.hpp
+++ subsystem-arm/implementations/mpu-router.hpp
@@ -8,15 +8,45 @@ namespace sjsu::arm
 {
     class MpuRouter
     {
-        MpuRouter(Mpu6050 &rotunda, Mpu6050 &shoulder, Mpu6050 &elbow, Mpu6050 &wrist) :
-        rotunda_(rotunda), shoulder_(shoulder), elbow_(elbow), wrist_(wrist)
+        MpuRouter(Mpu6050 &rotunda, Mpu6050 &shoulder, Mpu6050 &elbow, Mpu6050 &wrist) : rotunda_(rotunda), shoulder_(shoulder), elbow_(elbow), wrist_(wrist)
         {
             // TODO
         }
 
+        void InitializeAcclerometer()
+        {
+            rotunda_.ModuleInitialize();
+            shoulder_.ModuleInitialize();
+            elbow_.ModuleInitialize();
+            wrist_.ModuleInitialize();
+        }
+
         imu_accelerometers_feedback GetFeedback()
         {
-            return {};
+            imu_accelerometers_feedback temp;
+
+            Accelerometer::Acceleration_t rotunda_mpu = rotunda_.Read();
+            Accelerometer::Acceleration_t shoulder_mpu = shoulder_.Read();
+            Accelerometer::Acceleration_t elbow_mpu = elbow_.Read();
+            Accelerometer::Acceleration_t wrist_mpu = wrist_.Read();
+
+            temp.rotunda.x = static_cast<int>(rotunda_mpu.x);
+            temp.rotunda.y = static_cast<int>(rotunda_mpu.y);
+            temp.rotunda.z = static_cast<int>(rotunda_mpu.z);
+
+            temp.shoulder.x = static_cast<int>(shoulder_mpu.x);
+            temp.shoulder.y = static_cast<int>(shoulder_mpu.y);
+            temp.shoulder.z = static_cast<int>(shoulder_mpu.z);
+
+            temp.elbow.x = static_cast<int>(elbow_mpu.x);
+            temp.elbow.y = static_cast<int>(elbow_mpu.y);
+            temp.elbow.z = static_cast<int>(elbow_mpu.z);
+
+            temp.wrist.x = static_cast<int>(wrist_mpu.x);
+            temp.wrist.y = static_cast<int>(wrist_mpu.y);
+            temp.wrist.z = static_cast<int>(wrist_mpu.z);
+
+            return temp;
         }
 
     private:
