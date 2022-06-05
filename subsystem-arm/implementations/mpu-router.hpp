fa51cdabca27d8b32903fc5b0b555e780ae70e9e
Corey Kelley <coreyboy1820@hotmail.com>
2022-06-05 10:52:49 -0700

    changed mpu router

--- subsystem-arm/implementations/mpu-router.hpp
+++ subsystem-arm/implementations/mpu-router.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include "../library/devices/sensors/movement/accelerometer/mpu6050.hpp"
+#include "../library/devices/sensors/movement/accelerometer.hpp"
 #include "../subsystem-arm/dto/arm-dto.hpp"
 #include "utility/math/units.hpp"
 #include "utility/log.hpp"
@@ -8,7 +8,7 @@ namespace sjsu::arm
 {
     class MpuRouter
     {
-        MpuRouter(Mpu6050 &rotunda, Mpu6050 &shoulder, Mpu6050 &elbow, Mpu6050 &wrist) :
+        MpuRouter(Accelerometer &rotunda, Accelerometer &shoulder, Accelerometer &elbow, Accelerometer &wrist) :
         rotunda_(rotunda), shoulder_(shoulder), elbow_(elbow), wrist_(wrist)
         {
             // TODO
@@ -20,9 +20,9 @@ namespace sjsu::arm
         }
 
     private:
-        Mpu6050 &rotunda_;
-        Mpu6050 &shoulder_;
-        Mpu6050 &elbow_;
-        Mpu6050 &wrist_;
+        Accelerometer &rotunda_;
+        Accelerometer &shoulder_;
+        Accelerometer &elbow_;
+        Accelerometer &wrist_;
     };
 }
