84a71901f75a68ebd2829a134fdbaa2466192857
Coreyboy1820 <92711317+Coreyboy1820@users.noreply.github.com>
2022-06-05 13:06:00 -0700

    Rules engine test corey kelley (#47)
    * Created rules engine for drive
    
    * tested rules engine and fixed some logic error
    
    * added hearbeat class
    
    * fixed it

---
+++ subsystem-drive/implementations/rules-engine.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "../dto/drive-dto.hpp"
+#include "../common/heartbeat.hpp"
+
+namespace sjsu::drive
+{
+    class RulesEngine
+    {
+        public:
+        static constexpr int max_speed = 100;
+        drive_commands Check(drive_commands commands)
+        {
+            if(!heartbeat.IsSyncedWithMissionControl(commands.heartbeat_count))
+            {
+                commands.speed = 0;
+                sjsu::LogInfo("Overriding speed");
+                return commands;
+            }
+            if(!commands.is_operational)
+            {
+                commands.speed = 0;
+                sjsu::LogInfo("System is not operational... overriding speed");
+            }
+            if(commands.speed > max_speed || commands.speed < -max_speed)
+            {
+                commands.speed = std::clamp(commands.speed, -max_speed, max_speed);
+                sjsu::LogInfo("Specified speed is too fast... clamping speed");
+            }
+            heartbeat.IncrementHeartbeatCount();
+            return commands;
+        }
+
+        private:
+        sjsu::common::Heartbeat heartbeat;
+    };
+}
--- subsystem-drive/project.mk
+++ subsystem-drive/project.mk
@@ -1,2 +1,3 @@
 TESTS += test/steer-modes_test.cpp
 TESTS += test/mode-switcher_test.cpp
+TESTS += test/rules-engine_test.cpp
