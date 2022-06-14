ff8d532efc3cee1fb2d0fdff99332fd23af32815
naterpotatoers <nate.garza@hotmail.com>
2022-06-13 22:34:35 -0700

    fix broken parse test

--- subsystem-arm/implementations/mission-control-handler.hpp
+++ subsystem-arm/implementations/mission-control-handler.hpp
@@ -9,36 +9,33 @@ namespace sjsu::arm
     class MissionControlHandler
     {
     public:
+        // TODO
         arm_commands ParseMissionControlData(std::string &response)
         {
-              int actual_arguments = sscanf(
+            int actual_arguments = sscanf(
                 response.c_str(), response_body_format,
                 &commands.rotunda_angle, &commands.shoulder_angle, &commands.elbow_angle,
                 &commands.wrist_pitch_angle, &commands.wrist_yaw_angle, &commands.mode);
 
             return commands;
         }
-        std::string CreateGETRequestParameterWithRoverStatus( imu_accelerometers_feedback mpus , motors_feedback motors, hand_arguments hand, arm_commands arm_)
+        std::string CreateGETRequestParameterWithRoverStatus(imu_accelerometers_feedback mpus, motors_feedback motors, hand_arguments hand, arm_commands arm_)
         {
-            snprintf( 
-                request_parameter, 504,
-                "?heartbeat_count=0&is_operational=0&arm_mode=%c&hand_mode=%c&rotunda_speed=%d"
-                "&shoulder_speed%d&elbow_speed%d&wrist_speed%d&battery=0&rotunda_angle=%d&shoulder_angle=%d&elbow_angle=%d&wrist_"
-                "roll=%d&wrist_pitch=%d&pinky_angle=%d&ring_angle=%d&middle_angle=%d&"
-                "pointer_angle=%d&thumb_angle=%d"
-                "&rountda_accel_x=%d&rountda_accel_y=%d&rountda_accel_z=%d"
-                "&shoulder_accel_x=%d&shoulder_accel_y=%d&shoulder_accel_z=%d"
-                "&elbow_accel_x=%d&elbow_accel_y=%d&elbow_accel_z=%d"
-                "&wrist_accel_x=%d&wrist_accel_y=%d&wrist_accel_z=%d",
-                arm_.mode,hand.mode,
-                motors.rotunda_speed,motors.shoulder_speed,motors.elbow_speed,motors.wrist_speed,
-                arm_.rotunda_angle,arm_.shoulder_angle,arm_.elbow_angle,arm_.wrist_yaw_angle,arm_.wrist_pitch_angle,
-                hand.pinky_angle,hand.ring_angle,hand.middle_angle,hand.index_angle,hand.thumb_angle,
-                mpus.rotunda.x, mpus.rotunda.y, mpus.rotunda.z, 
-                mpus.shoulder.x, mpus.shoulder.y, mpus.shoulder.z, 
-                mpus.elbow.x, mpus.elbow.y, mpus.elbow.z, 
-                mpus.wrist.x, mpus.wrist.y, mpus.wrist.z
-                );
+            snprintf(
+                request_parameter, 600,
+                "?heartbeat_count=%d&is_operational=%d&arm_mode=%c&hand_mode=%c&rotunda_speed=%d"
+                "&shoulder_speed=%d&elbow_speed=%d&wrist_speed=%d&battery=0&rotunda_angle=%d&shoulder_angle=%d&elbow_angle=%d&wrist_"
+                "roll=%d&wrist_pitch=%d&pinky_angle=%d&ring_angle=%d&middle_angle=%d&pointer_angle=%d&thumb_angle=%d"
+                "&rountda_accel_x=%d&rountda_accel_y=%d&rountda_accel_z=%d&shoulder_accel_x=%d&shoulder_accel_y=%d&shoulder_accel_z=%d"
+                "&elbow_accel_x=%d&elbow_accel_y=%d&elbow_accel_z=%d&wrist_accel_x=%d&wrist_accel_y=%d&wrist_accel_z=%d",
+                arm_.heartbeat_count, arm_.is_operational, arm_.mode, hand.mode,
+                static_cast<int>(motors.rotunda_speed), static_cast<int>(motors.shoulder_speed), static_cast<int>(motors.elbow_speed), static_cast<int>(motors.wrist_speed),
+                arm_.rotunda_angle, arm_.shoulder_angle, arm_.elbow_angle, arm_.wrist_yaw_angle, arm_.wrist_pitch_angle,
+                hand.pinky_angle, hand.ring_angle, hand.middle_angle, hand.index_angle, hand.thumb_angle,
+                mpus.rotunda.x, mpus.rotunda.y, mpus.rotunda.z,
+                mpus.shoulder.x, mpus.shoulder.y, mpus.shoulder.z,
+                mpus.elbow.x, mpus.elbow.y, mpus.elbow.z,
+                mpus.wrist.x, mpus.wrist.y, mpus.wrist.z);
             return request_parameter;
         }
 
--- subsystem-arm/test/mission-control-handler_test.cpp
+++ subsystem-arm/test/mission-control-handler_test.cpp
@@ -15,15 +15,14 @@ namespace sjsu::arm
 
         SECTION("should return default arm status on startup")
         {
-            std::string expected_formatted_rover_status =  
-                "?heartbeat_count=0&is_operational=0&arm_mode=%c&hand_mode=%c&rotunda_speed=%d"
-                "&shoulder_speed%d&elbow_speed%d&wrist_speed%d&battery=0&rotunda_angle=%d&shoulder_angle=%d&elbow_angle=%d&wrist_"
-                "roll=%d&wrist_pitch=%d&pinky_angle=%d&ring_angle=%d&middle_angle=%d&"
-                "pointer_angle=%d&thumb_angle=%d"
-                "&rountda_accel_x=%d&rountda_accel_y=%d&rountda_accel_z=%d"
-                "&shoulder_accel_x=%d&shoulder_accel_y=%d&shoulder_accel_z=%d"
-                "&elbow_accel_x=%d&elbow_accel_y=%d&elbow_accel_z=%d"
-                "&wrist_accel_x=%d&wrist_accel_y=%d&wrist_accel_z=%d";
+            std::string expected_formatted_rover_status =
+                "?heartbeat_count=0&is_operational=0&arm_mode=A&hand_mode=A&rotunda_speed=0"
+                "&shoulder_speed=0&elbow_speed=0&wrist_speed=0&battery=0&rotunda_angle=0&shoulder_angle=0&elbow_angle=0&wrist_"
+                "roll=0&wrist_pitch=0&pinky_angle=0&ring_angle=0&middle_angle=0&"
+                "pointer_angle=0&thumb_angle=0&rountda_accel_x=0&rountda_accel_y=0&rountda_accel_z=0"
+                "&shoulder_accel_x=0&shoulder_accel_y=0&shoulder_accel_z=0"
+                "&elbow_accel_x=0&elbow_accel_y=0&elbow_accel_z=0"
+                "&wrist_accel_x=0&wrist_accel_y=0&wrist_accel_z=0";
 
             std::string actual_formatted_rover_status = mc_handler.CreateGETRequestParameterWithRoverStatus(mpu_feedback, arm_motors_feedback, hand_feedback, arm_feedback);
 
