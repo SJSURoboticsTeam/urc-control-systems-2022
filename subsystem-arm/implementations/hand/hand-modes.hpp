b6811fc44ef43fa2e5563debe4f85eb4f05ae4ea
acarrou <dridriswim@gmail.com>
2022-06-29 10:41:28 -0700

    adds pca9685 demo and working angle to pulse func in hand modes

--- subsystem-arm/implementations/hand/hand-modes.hpp
+++ subsystem-arm/implementations/hand/hand-modes.hpp
@@ -5,8 +5,12 @@ namespace sjsu::arm
 {
     class HandModes
     {
-        public:
-        HandModes()
-        {};
+    public:
+        HandModes(){};
+
+        std::chrono::microseconds AngleToPulseWidth(int angle)
+        {
+            return std::chrono::microseconds(angle * (4096 / 360));
+        }
     };
 }
