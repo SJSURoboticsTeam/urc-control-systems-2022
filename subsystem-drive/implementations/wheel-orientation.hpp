f221f617cd66c7e735ddf409311d07b1ea443e89
Gene Luong <luonggene@gmail.com>
2022-06-28 21:07:40 -0700

    added some frames to class

--- subsystem-drive/implementations/wheel-orientation.hpp
+++ subsystem-drive/implementations/wheel-orientation.hpp
@@ -6,3 +6,23 @@ pass new tri. variable (must be able to run all the time)
 create a temp leg arg pass same tri. ad.
 
 */
+#pragma once
+#include "../dto/drive-dto.hpp"
+
+namespace sjsu::drive
+{
+    class WheelOrientation
+    {
+        public:
+
+        private:
+        tri_wheel_router_arguments ligma;
+        tri_wheel_router_arguments step_up(int pos){
+
+        }
+        tri_wheel_router_arguments step_down(int pos){
+
+        }
+        int position = 0;
+    };
+}
