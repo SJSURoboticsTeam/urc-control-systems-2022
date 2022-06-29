46024ec33a23fb35e5b5c82e4eba37c69f61ff94
acarrou <dridriswim@gmail.com>
2022-06-28 21:57:44 -0700

    tests the PCA driver and initial implementation for the hand-router

--- subsystem-arm/implementations/hand/hand-router.hpp
+++ subsystem-arm/implementations/hand/hand-router.hpp
@@ -1,12 +1,54 @@
 #pragma once
 #include "dto/hand-dto.hpp"
+#include "utility/math/units.hpp"
+#include "../subsystem-arm/dto/arm-dto.hpp"
+#include "../implementations/hand/pca9685.hpp"
 
 namespace sjsu::arm
 {
     class HandRouter
     {
         public:
-        HandRouter()
-        {};
+        HandRouter(sjsu::Pca9685 &thumb, sjsu::Pca9685 &pointer, sjsu::Pca9685 &middle, sjsu::Pca9685 &ring, sjsu::Pca9685 &pinky) : 
+        thumb_(thumb), pointer_(pointer), middle_(middle), ring_(ring), pinky_(pinky) {};
+
+        //Initialize
+        void Initialize(){
+          thumb_.thumb.Initialize();
+          pointer_.pointer.Initialize();
+          middle_.middle.Initialize();
+          ring_.ring.Initialize();
+          pinky_.pinky.Initialize();
+
+        }
+        
+        //SetFingerArguments
+        arm_arguments SetFingerArguments(arm_arguments arguments){
+            thumb_,SetAngle(units::angle::degree_t(arguments.thumb_angle)), units::angular_velocity::revolutions_per_minute_t(arguments.speed));
+            pointer_.SetAngle(units::angle::degree_t(arguments.thumb_angle)), units::angular_velocity::revolutions_per_minute_t(arguments.speed));
+            arguments = arguments
+            return arguments;
+        }
+
+        //GetFingerArguments
+        arm_arguments GetFingerArguments(arm_argumen)
+    
+        //HomeFingers
+        void HomeFingers()
+        {
+            thumb_.setPulseWidth(1, 10ms)
+            pointer_.setPulseWidth(2, )
+            middle_.setPulseWidth(3, )
+            ring_.setPulseWidth(4, )
+            pinky_.setPulseWidth(5, )
+        }
+
+        private:
+        sjsu::arm::arm_arguments arguments_;
+        sjsu::Pca9685 &thumb_;
+        sjsu::Pca9685 &pointer_;
+        sjsu::Pca9685 &middle_;
+        sjsu::Pca9685 &ring_;
+        sjsu::Pca9685 &pinky_;
     };
 }
--- subsystem-arm/source/main.cpp
+++ subsystem-arm/source/main.cpp
@@ -1,62 +1,31 @@
-#include "utility/log.hpp"
+#include "../implementations/hand/pca9685.hpp"
 #include "peripherals/lpc40xx/i2c.hpp"
-#include "peripherals/lpc40xx/can.hpp"
-#include "peripherals/lpc17xx/pwm.hpp"
-#include "devices/actuators/servo/rmd_x.hpp"
-#include "../library/devices/sensors/movement/accelerometer/mpu6050.hpp"
-#include "dto/arm-dto.hpp"
-#include "../implementations/joints/mpu-router.hpp"
-#include "../implementations/joints/joint-router.hpp"
-#include "../implementations/mission-control-handler.hpp"
-
-
-
-
 
 int main()
 {
-  sjsu::LogInfo("Starting the rover arm system...");
-  sjsu::lpc40xx::Can & can = sjsu::lpc40xx::GetCan<2>();
-  sjsu::StaticMemoryResource<1024> memory_resource;
-  sjsu::CanNetwork can_network(can, &memory_resource);
 
-  // RMD addresses 0x141 - 0x148 are available
-  sjsu::RmdX rotunda_motor(can_network, 0x141);
-  sjsu::RmdX shoulder_motor(can_network, 0x142);
-  sjsu::RmdX elbow_motor(can_network, 0x143);
-  sjsu::RmdX left_wrist_motor(can_network, 0x144);
-  sjsu::RmdX right_wrist_motor(can_network, 0x145);
+    // Initializing 12c and pca9685
+    sjsu::lpc40xx::I2c &i2c = sjsu::lpc40xx::GetI2c<2>();
+    sjsu::Pca9685 pca9685(i2c);
+    sjsu::LogInfo("Initializing PCA...");
+    pca9685.Initialize();
+    pca9685.ModuleInitialize();
+    pca9685.setFrequency(60_Hz);
+    pca9685.SetDutyCycle(0, 0.2, 0);
+    pca9685.SetDutyCycle(1, 0.2, 0);
 
-  rotunda_motor.settings.gear_ratio     = 8;
-  shoulder_motor.settings.gear_ratio    = 8*65/16;  //gear ratio of motor times gear ratio of shoulder
-  elbow_motor.settings.gear_ratio       = 8*5/2;    //gear ratio of motor times gear ratio of elbow
-  left_wrist_motor.settings.gear_ratio  = 8;
-  right_wrist_motor.settings.gear_ratio = 8;
-
-  sjsu::arm::JointRouter arm(rotunda_motor, shoulder_motor, elbow_motor, left_wrist_motor, right_wrist_motor);
-  sjsu::arm::MissionControlHandler mc_handler;
-
-  sjsu::arm::arm_arguments arm_arguments;
-
-  arm.Initialize();
-  sjsu::LogInfo("Testing arm now");
-  int i;
-  while (true)
-  {
-    // arm_arguments = mc_handler(arm_arguments);
-    for(i = 0; i < 25; i++){
-    arm.SetArmArguments({0, i, 0, 0, 0});
-    sjsu::Delay(500ms);
-    for(i; i > 0; i--){
-    arm.SetArmArguments({0, i, 0, 0, 0});
-    sjsu::Delay(500ms);
+    while (1)
+    {
+        sjsu::LogInfo("Testing PCA...");
+        //pca9685.fullOn(0);
+        //pca9685.fullOn(1);
+        pca9685.setPulseWidth(0, 650us);
+        pca9685.setPulseWidth(1, 650us);
+        sjsu::Delay(2s);
+        //pca9685.fullOff(0);
+        //pca9685.fullOff(1);
+        pca9685.setPulseWidth(0, 2350us);
+        pca9685.setPulseWidth(1, 2350us);
     }
-    // sjsu::arm::arm_arguments arguments{5, 0, 20, 0, 0};
-    // arm.SetArmArguments(arguments);
-    // sjsu::Delay(3s);
-    // sjsu::arm::arm_arguments arguments2{25, -10, 0, 0, 0};
-    // arm.SetArmArguments(arguments);
-    // sjsu::Delay(3s);
-  }
-  }
+    return 0;
 }
