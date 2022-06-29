4204c580467dc7a1a54d06ee233132435bda99ef
Gene Luong <luonggene@gmail.com>
2022-06-28 20:59:02 -0700

    added some brain stormed ideas of wheels

---
+++ subsystem-drive/implementations/wheel-orientation.hpp
@@ -0,0 +1,8 @@
+/*
+IDEAS:
+
+Switch leg args. in tri addresses
+pass new tri. variable (must be able to run all the time)
+create a temp leg arg pass same tri. ad.
+
+*/
---
+++ subsystem-drive/test/wheel-orientation_test.cpp
@@ -0,0 +1,11 @@
+#include "testing/testing_frameworks.hpp"
+#include "utility/log.hpp"
+#include "implementations/wheel-orientation.hpp"
+
+TEST_CASE("Tri wheel router testing")
+{
+    SECTION("")
+    {
+
+    }
+}
