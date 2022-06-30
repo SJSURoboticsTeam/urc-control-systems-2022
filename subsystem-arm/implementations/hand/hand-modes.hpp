9517ca6b6f5f8de3c8da5a7cad79b93790a38ab5
Adrien Carrou <50222631+acarrou@users.noreply.github.com>
2022-06-29 21:03:07 -0700

    Adrien arm modes select (#85)
    * tests the PCA driver and initial implementation for the hand-router
    
    * adds pca9685 demo and working angle to pulse func in hand modes
    
    * file changes
    
    * initial commit to modes and mode select for hand
    
    * fixes hand args
    
    * Update hand-router.hpp

--- subsystem-arm/implementations/hand/hand-modes.hpp
+++ subsystem-arm/implementations/hand/hand-modes.hpp
@@ -1,12 +1,47 @@
 #pragma once
 #include "dto/hand-dto.hpp"
+#include "peripherals/lpc40xx/i2c.hpp"
+#include "pca9685.hpp"
 
 namespace sjsu::arm
 {
     class HandModes
     {
-        public:
-        HandModes()
-        {};
+    public:
+
+        static hand_arguments CloseHand(hand_arguments arguments_)
+        {
+            arguments_.pinky_angle = min_angle_;
+            arguments_.ring_angle = min_angle_;
+            arguments_.middle_angle = min_angle_;
+            arguments_.index_angle = min_angle_;
+            arguments_.thumb_angle = min_angle_;
+            return arguments_;
+        }
+
+        static hand_arguments OpenHand(hand_arguments arguments_)
+        {
+            arguments_.pinky_angle = max_angle_;
+            arguments_.ring_angle = max_angle_;
+            arguments_.middle_angle = max_angle_;
+            arguments_.index_angle = max_angle_;
+            arguments_.thumb_angle = max_angle_;
+            return arguments_;
+        }
+
+        static hand_arguments PointHand(hand_arguments arguments_)
+        {
+            arguments_.pinky_angle = min_angle_;
+            arguments_.ring_angle = min_angle_;
+            arguments_.middle_angle = min_angle_;
+            arguments_.index_angle = neutral_angle_;
+            arguments_.thumb_angle = max_angle_;
+            return arguments_;
+        }
+
+    private:
+        int min_angle_     = 30;
+        int neutral_angle_ = 90;
+        int max_angle_     = 180;
     };
 }
--- subsystem-arm/implementations/hand/hand-router.hpp
+++ subsystem-arm/implementations/hand/hand-router.hpp
@@ -1,12 +1,37 @@
 #pragma once
 #include "dto/hand-dto.hpp"
+#include "utility/math/units.hpp"
+#include "../subsystem-arm/dto/arm-dto.hpp"
+#include "../implementations/hand/pca9685.hpp"
 
 namespace sjsu::arm
 {
     class HandRouter
     {
-        public:
-        HandRouter()
-        {};
+    public:
+        HandRouter(sjsu::Pca9685 &pca_module) : pca_module_(pca_module){};
+
+        // Initialize
+        void Initialize()
+        {
+            pca_module_.ModuleInitialize();
+        }
+
+        std::chrono::microseconds AngleToPulseWidth(int angle)
+        {
+            return std::chrono::microseconds(angle * (4096 / 360));
+        }
+
+        void MoveToAngle(hand_arguments)
+        {
+            pca_module_.setPulseWidth(0, AngleToPulseWidth(hand_arguments_.pinky_angle));
+            pca_module_.setPulseWidth(1, AngleToPulseWidth(hand_arguments_.ring_angle));
+            pca_module_.setPulseWidth(2, AngleToPulseWidth(hand_arguments_.middle_angle));
+            pca_module_.setPulseWidth(3, AngleToPulseWidth(hand_arguments_.index_angle));
+            pca_module_.setPulseWidth(4, AngleToPulseWidth(hand_arguments_.thumb_angle));
+        }
+    private:
+        //sjsu::arm::arm_arguments arguments_;
+        hand_arguments hand_arguments_;
     };
 }
