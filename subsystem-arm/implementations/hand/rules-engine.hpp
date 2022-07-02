454d2602a06e01dbed76c8718a5674d2959325b0
Alexander Pereira <31743078+50xp50@users.noreply.github.com>
2022-07-02 12:27:16 -0700

    Hand rules engine (#82)
    * Added basic rules engine for hand, updated log info for joint
    
    * add unit test for hand rules engine
    
    * Added test for is_operational in rules engine for hand
    
    * add unit test for clamp finger angles
    
    Co-authored-by: naterpotatoers <nate.garza@hotmail.com>

--- subsystem-arm/implementations/hand/rules-engine.hpp
+++ subsystem-arm/implementations/hand/rules-engine.hpp
@@ -1,12 +1,39 @@
 #pragma once
-#include "dto/hand-dto.hpp"
+#include "utility/log.hpp"
+#include "../../dto/arm-dto.hpp"
+#include "../../common/heartbeat.hpp"
 
-namespace sjsu::hand
+namespace sjsu::arm
 {
     class RulesEngine
     {
-        public:
-        RulesEngine(hand_arguments hand_args)
-        {};
+    public:
+        static constexpr int kMaxFingerAngle = 180;
+        static constexpr int kMinFingerAngle = 0;
+
+        hand_arguments ValidateCommands(hand_arguments commands)
+        {
+            if (!heartbeat_.IsSyncedWithMissionControl(commands.heartbeat_count))
+            {
+                sjsu::LogInfo("Hand was unsynced, holding previous state!");
+                return previous_commands;
+            }
+            if (!commands.is_operational)
+            {
+                sjsu::LogInfo("Hand is not operational... holding previous state!");
+                return previous_commands;
+            }
+            if (commands.index_angle > 180) // WIP: Potential issue with desyncing and previous commands not being clamped
+            {
+                commands.index_angle = std::clamp(commands.index_angle, kMinFingerAngle, kMaxFingerAngle);
+            }
+            heartbeat_.IncrementHeartbeatCount();
+            previous_commands = commands;
+            return commands;
+        };
+
+    private:
+        sjsu::common::Heartbeat heartbeat_;
+        hand_arguments previous_commands;
     };
 }
