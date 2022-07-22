6951fcee9b248c0b26e4f45db3b4ec0b70e5568c
acarrou <dridriswim@gmail.com>
2022-07-21 22:12:38 -0700

    working uart!

--- subsystem-drive/source/main.cpp
+++ subsystem-drive/source/main.cpp
@@ -1,20 +1,28 @@
 #include <cstdio>
+#include <iterator>
 
 #include "peripherals/lpc40xx/uart.hpp"
 #include "utility/log.hpp"
 #include "utility/time/time.hpp"
-#include "utility/log.hpp"
+
+const char response_body_format[] =
+    "\r\n\r\n{\n"
+    "  \"drive_mode\": \"%c\",\n"
+    "  \"speed\": %d,\n"
+    "  \"angle\": %d\n"
+    "  \"wheel_orientation\": %d,\n"
+    "}";
 
 int main()
 {
     sjsu::LogInfo("Starting application...");
-    sjsu::lpc40xx::Uart &uart2 = sjsu::lpc40xx::GetUart<0>();
+    auto &uart2 = sjsu::lpc40xx::GetUart<0>();
     uart2.settings.baud_rate = 38400;
     uart2.Initialize();
 
     while (1)
     {
-        std::array<uint8_t, 32> receive_buffer;
+        std::array<uint8_t, 64> receive_buffer;
         receive_buffer.fill(0);
 
         if (uart2.HasData())
