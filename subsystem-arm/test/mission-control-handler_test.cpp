3c3643c98e09818a8238aad96ea110df89fbbc67
mchan2003 <mchan100work@gmail.com>
2022-08-04 10:57:32 -0700

    fixed mission-control-handler_test error

--- subsystem-arm/test/mission-control-handler_test.cpp
+++ subsystem-arm/test/mission-control-handler_test.cpp
@@ -17,7 +17,7 @@ namespace sjsu::arm
         SECTION("should return default arm status on startup")
         {
             std::string expected_formatted_rover_status =
-                "?heartbeat_count=0&is_operational=0&arm_mode=A&hand_mode=A&rotunda_speed=0"
+                "?heartbeat_count=0&is_operational=0&arm_mode=A&hand_mode=I&rotunda_speed=0"
                 "&shoulder_speed=0&elbow_speed=0&wrist_speed=0&battery=0&rotunda_angle=0&shoulder_angle=0&elbow_angle=0&wrist_"
                 "roll=0&wrist_pitch=0&pinky_angle=0&ring_angle=0&middle_angle=0&"
                 "pointer_angle=0&thumb_angle=0&rountda_accel_x=0&rountda_accel_y=0&rountda_accel_z=0"
