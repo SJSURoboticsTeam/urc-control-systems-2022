397031357528c59e8d1e811db12c20e9360e2aef
naterpotatoers <nate.garza@hotmail.com>
2022-08-27 15:28:52 -0700

    working elbow motor test

--- demos/rmd-arm/source/main.cpp
+++ demos/rmd-arm/source/main.cpp
@@ -11,21 +11,21 @@ int main()
     // sjsu::StaticMemoryResource<1024> memory_resource;
     // sjsu::CanNetwork can_network(can, &memory_resource);
 
-    // sjsu::RmdX shoulder_motor(can_network, 0x142);
+    // sjsu::RmdX elbow_motor(can_network, 0x142);
 
-    // shoulder_motor.settings.gear_ratio = 8 * 65 / 16; // gear ratio of motor times gear ratio of shoulder
+    // elbow_motor.settings.gear_ratio = 8 * 65 / 16; // gear ratio of motor times gear ratio of shoulder
 
     // // rotunda_motor.Initialize();
-    // shoulder_motor.Initialize();
+    // elbow_motor.Initialize();
     // // left_wrist_motor.Initialize();
     // // right_wrist_motor.Initialize();
 
     // while (true)
     // {
     //     sjsu::LogInfo("Moving motor...");
-    //     shoulder_motor.GetFeedback().Print();
-    //     auto feedback = shoulder_motor.GetFeedback().missed_feedback;
-    //     shoulder_motor.SetSpeed(5_rpm);
+    //     elbow_motor.GetFeedback().Print();
+    //     auto feedback = elbow_motor.GetFeedback().missed_feedback;
+    //     elbow_motor.SetSpeed(5_rpm);
     //     sjsu::Delay(100ms);
     // }
 
@@ -35,7 +35,7 @@ int main()
     sjsu::CanNetwork can_network(can, &memory_resource);
 
     // RMD addresses 0x141 - 0x148 are available
-    sjsu::RmdX shoulder_motor(can_network, 0x143);
+    sjsu::RmdX elbow_motor(can_network, 0x143);
     // sjsu::RmdX wrist_motor1(can_network, 0x144);
     // sjsu::RmdX wrist_motor2(can_network, 0x145);
 
@@ -43,10 +43,10 @@ int main()
 
     // wrist_motor1.settings.gear_ratio = 8 * 65 / 16; // gear ratio of motor times gear ratio of shoulder
     // wrist_motor2.settings.gear_ratio = 8 * 65 / 16; // gear ratio of motor times gear ratio of shoulder
-    shoulder_motor.settings.gear_ratio = 8 * 65 / 16; // gear ratio of motor times gear ratio of shoulder
+    elbow_motor.settings.gear_ratio = 8 * 65 / 16; // gear ratio of motor times gear ratio of shoulder
     // elbow_motor.settings.gear_ratio = 8 * 5 / 2; // gear ratio of motor times gear ratio of elbow
 
-    shoulder_motor.Initialize();
+    elbow_motor.Initialize();
     // elbow_motor.Initialize();
     // wrist_motor1.Initialize();
     // wrist_motor2.Initialize();
@@ -69,11 +69,17 @@ int main()
         // elbow_motor.SetSpeed(-5_rpm);
         // sjsu::Delay(600ms);
 
-        shoulder_motor.GetFeedback().Print();
-        shoulder_motor.SetSpeed(5_rpm);
-        sjsu::Delay(500ms);
-        shoulder_motor.SetSpeed(-5_rpm);
-        sjsu::Delay(600ms);
+        elbow_motor.GetFeedback().Print();
+        elbow_motor.SetAngle(10_deg, 3_rpm);
+        sjsu::Delay(3000ms);
+        elbow_motor.SetAngle(-10_deg, 3_rpm);
+        sjsu::Delay(3000ms);
+
+        // elbow_motor.GetFeedback().Print();
+        // elbow_motor.SetSpeed(5_rpm);
+        // sjsu::Delay(500ms);
+        // elbow_motor.SetSpeed(-5_rpm);
+        // sjsu::Delay(600ms);
 
         // sjsu::LogInfo("Moving elbow...");
         // elbow_motor.GetFeedback().Print();
