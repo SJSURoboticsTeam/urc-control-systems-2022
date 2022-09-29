3a48652052e1f8212484952adb02b45de17961bd
naterpotatoers <nate.garza@hotmail.com>
2022-09-28 23:10:48 -0700

    fixes esp class in common and updates esp demo working w web server /drive

--- common/esp.hpp
+++ common/esp.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <string_view>
+#include <cinttypes>
+#include <cstdint>
 
 #include "utility/log.hpp"
 #include "peripherals/lpc40xx/uart.hpp"
@@ -14,84 +15,77 @@ namespace sjsu::common
         Esp()
             : esp_(sjsu::lpc40xx::GetUart<3>()),
               wifi_(esp_.GetWiFi()),
-              socket_(esp_.GetInternetSocket()) { esp_.Initialize(); };
+              socket_(esp_.GetInternetSocket())
+        {
+            esp_.Initialize();
+        };
 
         void Connect()
         {
-            ConnectToWifi();
-            ConnectToWebServer();
+            try
+            {
+                sjsu::LogInfo("Connecting to %s...", kSsid);
+                wifi_.ConnectToAccessPoint(kSsid, kPassword, kDefaultTimeout);
+                sjsu::LogInfo("Connecting to Socket...");
+                socket_.Connect(sjsu::InternetSocket::Protocol::kTCP, kUrl, kPort, kDefaultTimeout);
+                sjsu::LogInfo("Connected!");
+            }
+            catch (const std::exception &e)
+            {
+                sjsu::LogError("Error connecting to Wifi!");
+            }
         }
 
         void Disconnect()
         {
-            wifi_.DisconnectFromAccessPoint();
-        }
-        /// Verifies that the Wi-Fi module is still connected to the network
-        /// @return true if the module is still connected to the internet
-        bool IsConnected()
-        {
-            return wifi_.IsConnected(); // TODO: Always returns false
-        };
-
-        std::string GetCommands(std::string request)
-        {
             try
             {
-                request = "GET /drive?param=0 HTTP/1.1\r\nHost: " + kUrl + "\r\n\r\n";
-                std::span request_payload(
-                    reinterpret_cast<const uint8_t *>(request.data()), request.size());
+                sjsu::LogInfo("Disconnecting from Socket...");
+                socket_.Close();
+                sjsu::LogInfo("Disconnecting from Wifi...");
+                wifi_.DisconnectFromAccessPoint();
+                sjsu::LogInfo("Disconnected!");
+            }
+            catch (const std::exception &e)
+            {
+                sjsu::LogError("Error disconnecting from Wifi!");
+            }
+        }
+
+        std::string GetCommands(char *endpoint)
+        {
+            std::array<uint8_t, 1024 * 2> raw_response;
+            std::fill(raw_response.begin(), raw_response.end(), 0);
+
+            char request[200];
+            snprintf(request, sizeof(request), "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", endpoint, kUrl);
+
+            std::span request_payload(reinterpret_cast<const uint8_t *>(request), sizeof(request));
+
+            try
+            {
                 socket_.Write(request_payload, kDefaultTimeout);
-                std::array<uint8_t, 1024 * 2> raw_response;
-                std::fill(raw_response.begin(), raw_response.end(), 0);
-                size_t read_back = socket_.Read(raw_response, kDefaultTimeout);
-                std::string str_response(reinterpret_cast<char *>(raw_response.data()), read_back);
+                size_t response_size = socket_.Read(raw_response, kDefaultTimeout);
+                std::string str_response(reinterpret_cast<char *>(raw_response.data()), response_size);
                 return str_response;
             }
             catch (const std::exception &e)
             {
-                sjsu::LogError("Error getting commands!");
+                sjsu::LogError("Error writing/reading commands!");
+                Disconnect();
+                Connect();
+                return "";
             }
         }
 
     private:
-        void ConnectToWifi()
-        {
-            try
-            {
-                printf("Attempting to connect to %s...", kSsid);
-                while (!wifi_.ConnectToAccessPoint(kSsid, kPassword, kDefaultTimeout))
-                {
-                    printf(".");
-                }
-                printf(" Connected!\n");
-            }
-            catch (const std::exception &e)
-            {
-                sjsu::LogError("Error connecting to wifi!");
-            }
-        }
-
-        void ConnectToWebServer()
-        {
-            try
-            {
-                sjsu::LogInfo("Connecting to web server: %s:%s...", kUrl, kPort);
-                socket_.Connect(sjsu::InternetSocket::Protocol::kTCP, kUrl,
-                                static_cast<uint16_t>(kPort), kDefaultTimeout);
-            }
-            catch (const std::exception &e)
-            {
-                sjsu::LogError("Error connecting to server!");
-            }
-        }
-
         sjsu::Esp8266 esp_;
         sjsu::WiFi &wifi_;
         sjsu::InternetSocket &socket_;
         const uint16_t kPort = 5000;
-        const std::string kUrl = "172.23.163.9";
-        const std::string kSsid = "Adrien";
-        const std::string kPassword = "LetsFly!";
-        std::chrono::seconds kDefaultTimeout = 10s;
+        const char *kUrl = "192.168.1.28";
+        const char *kSsid = "Izzys 2.4GHz";
+        const char *kPassword = "Nezzy559";
+        std::chrono::seconds kDefaultTimeout = 5s;
     };
 } // namespace sjsu::common
