9342c3211e448851338181ff3badffc6835d0cfe
Ryan <reluc082092@gmail.com>
2022-10-10 23:15:09 -0700

    Added lockout angles dependent on where the elbow and rotunda are to make sure arm will not hurt itself

--- demos/mpu-arm/source/main.cpp
+++ demos/mpu-arm/source/main.cpp
@@ -9,12 +9,10 @@ void PrintAcceleration(sjsu::Mpu6050 &sensor)
     current_acceleration.Print();
 }
 
-void InitializeArm(sjsu::Mpu6050 &rotunda, sjsu::Mpu6050 &shoulder, sjsu::Mpu6050 &elbow, sjsu::Mpu6050 &wrist)
+void InitializeArm( sjsu::Mpu6050 &elbow)
 {
-    rotunda.Initialize();
-    shoulder.Initialize();
     elbow.Initialize();
-    wrist.Initialize();
+
 }
 
 int main()
@@ -22,24 +20,18 @@ int main()
     sjsu::LogInfo("Mpu6050 Application Starting...");
 
     sjsu::lpc40xx::I2c &i2c = sjsu::lpc40xx::GetI2c<2>();
-    sjsu::Mpu6050 rotunda(i2c, 0x66);
-    sjsu::Mpu6050 shoulder(i2c, 0x67);
     sjsu::Mpu6050 elbow(i2c, 0x68);
-    sjsu::Mpu6050 wrist(i2c, 0x69);
+
 
     sjsu::LogInfo("Initializing accelerometers...");
-    InitializeArm(rotunda, shoulder, elbow, wrist);
+    InitializeArm(elbow);
 
     while (true)
     {
-        sjsu::LogInfo("Reading rotunda Mpu6050 acceleration data...");
-        PrintAcceleration(rotunda);
-        sjsu::LogInfo("Reading shoulder Mpu6050 acceleration data...");
-        PrintAcceleration(shoulder);
+
         sjsu::LogInfo("Reading elbow Mpu6050 acceleration data...");
         PrintAcceleration(elbow);
-        sjsu::LogInfo("Reading wrist Mpu6050 acceleration data...");
-        PrintAcceleration(wrist);
+
 
         sjsu::Delay(100ms);
     }
--- subsystem-arm/implementations/rules-engine.hpp
+++ subsystem-arm/implementations/rules-engine.hpp
@@ -12,8 +12,13 @@ namespace sjsu::arm
         static constexpr int kMaxSpeed = 20;
         static constexpr int kMinFingerAngle = 88;
         static constexpr int kMaxFingerAngle = 175;
-        static constexpr int kMaxShoulderAngle = 90;
-
+        
+        static constexpr int kRotundaAngle = 480;
+        int kMaxShoulderAngle = 90;
+        int kMinShoulderAngle = 90;
+        int kMaxElbowAngle = 90;
+        int kMinElbowAngle = 90; 
+        int kMaxWristPitchAngle = 20;
         arm_arguments ValidateCommands(arm_arguments commands)
         {
             // if(!heartbeat_.IsSyncedWithMissionControl(commands.joint_args.heartbeat_count))
@@ -36,6 +41,20 @@ namespace sjsu::arm
             commands.hand_args.thumb_angle = std::clamp(commands.hand_args.thumb_angle, kMinFingerAngle, kMaxFingerAngle);
 
             commands.joint_args.speed = std::clamp(commands.joint_args.speed, -kMaxSpeed, kMaxSpeed);
+            
+            //To prevent wire strangulation of via the rotunda
+            commands.joint_args.rotunda_angle = std::clamp(commands.joint_args.rotunda_angle,-kRotundaAngle,kRotundaAngle;)
+
+            // if elbow goes below the body
+            if(commands.joint_args.shoulder_angle < 45)
+                //block the elbow from going below a certain angle
+                kMinElbowAngle = -45
+            if(commands.joint_args.shoulder_angle < -45)
+                kMaxElbowAngle = 45;
+
+
+
+            commands.joint_args.wrist_pitch_angle
 
             heartbeat_.IncrementHeartbeatCount();
             return commands;
