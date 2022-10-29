384285ffda4cf0a90e293b6947625a1d0728fd51
Viha123 <sviha195@gmail.com>
2022-10-29 14:34:33 -0700

    start and stop for servo

--- demos/rr9-demo/source/main.cpp
+++ demos/rr9-demo/source/main.cpp
@@ -15,13 +15,14 @@ int main()
     sjsu::Servo servo(pwm);
     sjsu::LogInfo("Setting Servo pulse width bounds from 500us to 2500us.");
     // documentation says 1.5ms is neutral, but 1.5ms is 1500us
-    servo.settings.min_pulse = 1300us;
-    servo.settings.max_pulse = 1700us;
+    servo.settings.min_pulse = 500us;
+    servo.settings.max_pulse = 2500us;
 
     // Set the angle bounds of the servo to be 0 degrees and 180 degrees
     sjsu::LogInfo("Setting Servo angle bounds from 0 deg to 180 deg.");
     servo.settings.min_angle = 0_deg;
-    servo.settings.max_angle = 180_deg;
+    servo.settings.max_angle = 200_deg;
+    // servo max angle is at 173, 173> makes go servo crazy, right now servo ossicilates betweem 0--> 173
 
     sjsu::LogInfo("Initalizing Servo");
     servo.Initialize();
@@ -32,23 +33,33 @@ int main()
 
     while (true)
     {
-        std::string response = serial.GetCommands();
-        if (response != "")
-        {
-            printf("Received: %s\n", response.c_str());
-            entered_angle = std::stoi(response);
-        }
-        units::angle::degree_t desired_angle(entered_angle);
-        sjsu::LogInfo("Setting servo to %d degrees", entered_angle);
-        servo.SetAngle(desired_angle);
-
-        // this is more realistic
-        // for (units::angle::degree_t servo_angle = 0_deg; servo_angle < 180_deg; servo_angle++)
+        // std::string response = serial.GetCommands();
+        // if (response != "")
         // {
-        //     sjsu::LogInfo("Setting servo to %f degrees", servo_angle.to<double>());
-        //     servo.SetAngle(servo_angle);
-        //     sjsu::Delay(27ms);
+        //     printf("Received: %s\n", response.c_str());
+        //     entered_angle = std::stoi(response);
         // }
+        // units::angle::degree_t desired_angle(entered_angle);
+        // sjsu::LogInfo("Setting servo to %d degrees", entered_angle);
+        // servo.SetAngle(desired_angle);
+
+        //this is more realisticservo.settings.min_angle
+        // Open
+         printf("before 1st for\n");
+
+        for (units::angle::degree_t servo_angle = 0_deg; servo_angle < 65_deg; servo_angle++)
+        {
+            // printf("Setting servo to %f degrees\n\n", servo_angle.to<double>());
+            servo.SetAngle(servo_angle);
+            sjsu::Delay(30ms);
+        }
+         printf("before 2nd for\n");
+        for (units::angle::degree_t servo_angle = 65_deg; servo_angle > 0_deg; servo_angle--)
+        {
+            // printf("Setting servo to %f degrees\n\n", servo_angle.to<double>());
+            servo.SetAngle(servo_angle);
+            sjsu::Delay(30ms);
+        }
 
         // not sure why they did this but it was in the demo
         // for (std::chrono::microseconds pulse_width = 1700us; pulse_width > 1300us; pulse_width--)
