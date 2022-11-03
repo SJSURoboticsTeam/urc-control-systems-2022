1f26b37c68235a78b8f380aa5c33fb5792a618d1
Gene Luong <luonggene@gmail.com>
2022-11-02 21:14:12 -0700

    mux implemented

--- demos/mpu-arm/source/main.cpp
+++ demos/mpu-arm/source/main.cpp
@@ -3,9 +3,9 @@
 #include "devices/sensors/movement/accelerometer/mpu6050.hpp"
 #include "utility/log.hpp"
 
-void PrintAcceleration(sjsu::Mpu6050 &sensor)
+void PrintAcceleration(sjsu::Mpu6050 &mpu)
 {
-    auto current_acceleration = sensor.Read();
+    auto current_acceleration = mpu.Read();
     current_acceleration.Print();
 }
 
@@ -22,22 +22,34 @@ int main()
     sjsu::LogInfo("Mpu6050 Application Starting...");
 
     sjsu::lpc40xx::I2c &i2c = sjsu::lpc40xx::GetI2c<2>();
-    sjsu::Mpu6050 rotunda(i2c, 0x66);
-    sjsu::Mpu6050 shoulder(i2c, 0x67);
+    uint8_t buff[1] = {0};
+
+    sjsu::Mpu6050 rotunda(i2c, 0x68);
+    sjsu::Mpu6050 shoulder(i2c, 0x68);
     sjsu::Mpu6050 elbow(i2c, 0x68);
-    sjsu::Mpu6050 wrist(i2c, 0x69);
+    sjsu::Mpu6050 wrist(i2c, 0x68);
 
     sjsu::LogInfo("Initializing accelerometers...");
     InitializeArm(rotunda, shoulder, elbow, wrist);
-
     while (true)
     {
+        buff[0] = 1;
+        i2c.Write(0x70, buff, 1); // set [00000001]
         sjsu::LogInfo("Reading rotunda Mpu6050 acceleration data...");
         PrintAcceleration(rotunda);
+
+        buff[0] = static_cast<uint8_t>(buff[0] << 1);
+        i2c.Write(0x70, buff, 1); // set [00000010]
         sjsu::LogInfo("Reading shoulder Mpu6050 acceleration data...");
         PrintAcceleration(shoulder);
+
+        buff[0] = static_cast<uint8_t>(buff[0] << 1);
+        i2c.Write(0x70, buff, 1); // set [00000100]
         sjsu::LogInfo("Reading elbow Mpu6050 acceleration data...");
         PrintAcceleration(elbow);
+        
+        buff[0] = static_cast<uint8_t>(buff[0] << 1); 
+        i2c.Write(0x70, buff, 1); // set [00001000]
         sjsu::LogInfo("Reading wrist Mpu6050 acceleration data...");
         PrintAcceleration(wrist);
 
