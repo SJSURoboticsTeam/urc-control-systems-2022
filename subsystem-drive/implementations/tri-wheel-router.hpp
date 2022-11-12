804e94f66054d98d24f76b9725ec69ba79cafa88
evanastle <starphys@gmail.com>
2022-11-12 13:00:25 -0800

    Move wheel offsets into leg struct

--- subsystem-drive/implementations/tri-wheel-router.hpp
+++ subsystem-drive/implementations/tri-wheel-router.hpp
@@ -21,6 +21,7 @@ namespace sjsu::drive
             sjsu::RmdX &steer_motor_;
             sjsu::RmdX &drive_motor_;
             sjsu::Gpio &magnet_;
+            int wheel_offset_;
         };
 
         TriWheelRouter(leg right, leg left, leg back) : left_(left), back_(back), right_(right)
@@ -41,19 +42,22 @@ namespace sjsu::drive
             left_.magnet_.SetAsInput();
             right_.magnet_.SetAsInput();
             back_.magnet_.SetAsInput();
+            left_.wheel_offset_ = 0;
+            right_.wheel_offset_ = 0;
+            back_.wheel_offset_ = 0;
         }
 
         tri_wheel_router_arguments SetLegArguments(tri_wheel_router_arguments tri_wheel_arguments)
         {
-            left_.steer_motor_.SetAngle(units::angle::degree_t(-tri_wheel_arguments.left.steer.angle + left_wheel_offset),
+            left_.steer_motor_.SetAngle(units::angle::degree_t(-tri_wheel_arguments.left.steer.angle + left_.wheel_offset),
                                         units::angular_velocity::revolutions_per_minute_t(tri_wheel_arguments.left.steer.speed));
             left_.drive_motor_.SetSpeed(units::angular_velocity::revolutions_per_minute_t(tri_wheel_arguments.left.hub.speed));
 
-            right_.steer_motor_.SetAngle(units::angle::degree_t(-tri_wheel_arguments.right.steer.angle + right_wheel_offset),
+            right_.steer_motor_.SetAngle(units::angle::degree_t(-tri_wheel_arguments.right.steer.angle + right_.wheel_offset),
                                          units::angular_velocity::revolutions_per_minute_t(tri_wheel_arguments.right.steer.speed));
             right_.drive_motor_.SetSpeed(units::angular_velocity::revolutions_per_minute_t(tri_wheel_arguments.right.hub.speed));
 
-            back_.steer_motor_.SetAngle(units::angle::degree_t(-tri_wheel_arguments.back.steer.angle + back_wheel_offset),
+            back_.steer_motor_.SetAngle(units::angle::degree_t(-tri_wheel_arguments.back.steer.angle + back_wheel_.offset),
                                         units::angular_velocity::revolutions_per_minute_t(tri_wheel_arguments.back.steer.speed));
             back_.drive_motor_.SetSpeed(units::angular_velocity::revolutions_per_minute_t(tri_wheel_arguments.back.hub.speed));
 
@@ -93,22 +97,22 @@ namespace sjsu::drive
                 sjsu::LogInfo("HomingPins L = %d\t R = %d\t B = %d", left_.magnet_.Read(), right_.magnet_.Read(), back_.magnet_.Read()); //sigma
                 if (left_.magnet_.Read() == not_homed)
                 {
-                    left_wheel_offset++;
-                    left_.steer_motor_.SetAngle(units::angle::degree_t(left_wheel_offset, 2_rpm));
+                    left_.wheel_offset++;
+                    left_.steer_motor_.SetAngle(units::angle::degree_t(left_.wheel_offset, 2_rpm));
                 }
 
                 if (right_.magnet_.Read() == not_homed)
                 {
-                    right_wheel_offset++;
-                    right_.steer_motor_.SetAngle(units::angle::degree_t(right_wheel_offset, 2_rpm));
+                    right_.wheel_offset++;
+                    right_.steer_motor_.SetAngle(units::angle::degree_t(right_.wheel_offset, 2_rpm));
                 }
 
                 if (back_.magnet_.Read() == not_homed)
                 {
-                    back_wheel_offset++;
-                    back_.steer_motor_.SetAngle(units::angle::degree_t(back_wheel_offset, 2_rpm));
+                    back_.wheel_offset++;
+                    back_.steer_motor_.SetAngle(units::angle::degree_t(back_.wheel_offset, 2_rpm));
                 }
-                sjsu::LogInfo("b = %d\tr = %d\tl = %d", back_wheel_offset, right_wheel_offset, left_wheel_offset);
+                sjsu::LogInfo("b = %d\tr = %d\tl = %d", back_.wheel_offset, right_.wheel_offset, left_.wheel_offset);
                 angle_verification = GetMotorFeedback();
                 while (angle_verification.left_steer_speed != 0_rpm || angle_verification.right_steer_speed != 0_rpm || angle_verification.back_steer_speed != 0_rpm)
                 {
@@ -128,9 +132,6 @@ namespace sjsu::drive
 
     private:
         // member variables
-        int16_t left_wheel_offset = 0;
-        int16_t right_wheel_offset = 0;
-        int16_t back_wheel_offset = 0;
 
         leg left_;
         leg back_;
