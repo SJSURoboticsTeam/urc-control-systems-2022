804708f2879d9123ff2ae6eac30e88b3119d5aad
FluffyFTW <46359603+FluffyFTW@users.noreply.github.com>
2022-11-12 16:37:17 -0700

    4 Mpu demo  (#198)
    * Dev mergeconflicts (#186)
    
    * fixes esp class in common and updates esp demo working w web server /drive (#151)
    
    * adds GPS demo (#145)
    
    * adds GPS demo
    
    * adds tested and working translate mode (#147)
    
    * Merging latest working development into main branch (#144)
    
    * fixes all broken unit tests on drive (#142)
    
    * Adrien latest working drive/arm (#143)
    
    * latest working arm and drive code used during club day
    
    * fixes arm mission control parsing
    
    * reverts substring drive parser
    
    Co-authored-by: Nathanael Garza <41933236+naterpotatoers@users.noreply.github.com>
    Co-authored-by: naterpotatoers <nate.garza@hotmail.com>
    
    * fixes variable name for back wheel offset (#148)
    
    * fixes esp class in common and updates esp demo working w web server /drive (#151)
    
    Co-authored-by: Nathanael Garza <41933236+naterpotatoers@users.noreply.github.com>
    Co-authored-by: naterpotatoers <nate.garza@hotmail.com>
    
    * deletes arm subfolder, major overhaul to structure, need testing (#152)
    
    * Mapped hand angles to go from 0 to 100. Any value below 0 will get ceiling to 0. Any value above 100 will get floored to 100
    
    * New hand demo for rr9 (#171)
    
    * adds rr9 demo
    
    * tested demo, motor stops when passed angle 100, moves clockwise when given angle < 0, moves cc when given angle > 120
    
    * fixed most type conversion (#179)
    
    * fixed most type conversion
    
    * mismatched mission control parser
    
    * Drive warnings (#184)
    
    * merge conflicts + variable name change
    
    * Fixing main control loop
    
    * fixed merge conflicts and arm main control loop works
    
    Co-authored-by: Nathanael Garza <41933236+naterpotatoers@users.noreply.github.com>
    Co-authored-by: Adrien <50222631+acarrou@users.noreply.github.com>
    Co-authored-by: naterpotatoers <nate.garza@hotmail.com>
    Co-authored-by: Ahmed Sabzwari <107161645+AhmedSabz@users.noreply.github.com>
    Co-authored-by: FluffyFTW <46359603+FluffyFTW@users.noreply.github.com>
    Co-authored-by: Gene Luong <luonggene@gmail.com>
    
    * Revert "Dev mergeconflicts (#186)" (#187)
    
    This reverts commit 84af8d4a8c93e1b3c43b0028180ec0a9dc518f4f.
    
    * mux implemented
    
    * tested
    
    * Fixed mux's access to addresses at different times
    
    * singular mpu tests
    
    * merge fix
    
    Co-authored-by: Coreyboy1820 <92711317+Coreyboy1820@users.noreply.github.com>
    Co-authored-by: Nathanael Garza <41933236+naterpotatoers@users.noreply.github.com>
    Co-authored-by: Adrien <50222631+acarrou@users.noreply.github.com>
    Co-authored-by: naterpotatoers <nate.garza@hotmail.com>
    Co-authored-by: Ahmed Sabzwari <107161645+AhmedSabz@users.noreply.github.com>
    Co-authored-by: coreyboy1820 <coreyboy1820@hotmail.com>

--- demos/mpu-arm/source/main.cpp
+++ demos/mpu-arm/source/main.cpp
@@ -3,18 +3,25 @@
 #include "devices/sensors/movement/accelerometer/mpu6050.hpp"
 #include "utility/log.hpp"
 
-void PrintAcceleration(sjsu::Mpu6050 &sensor)
+
+
+void PrintAcceleration(sjsu::Mpu6050 &mpu)
 {
-    auto current_acceleration = sensor.Read();
+    auto current_acceleration = mpu.Read();
     current_acceleration.Print();
 }
 
-void InitializeArm(sjsu::Mpu6050 &rotunda, sjsu::Mpu6050 &shoulder, sjsu::Mpu6050 &elbow, sjsu::Mpu6050 &wrist)
+void InitializeBase(sjsu::Mpu6050 &rotunda, sjsu::Mpu6050 &shoulder)
 {
     rotunda.Initialize();
     shoulder.Initialize();
-    elbow.Initialize();
+}
+
+void InitializeJoints(sjsu::Mpu6050 &elbow, sjsu::Mpu6050 &wrist)
+{
     wrist.Initialize();
+    sjsu::LogInfo("Wrist initalized");
+    elbow.Initialize();
 }
 
 int main()
@@ -22,26 +29,47 @@ int main()
     sjsu::LogInfo("Mpu6050 Application Starting...");
 
     sjsu::lpc40xx::I2c &i2c = sjsu::lpc40xx::GetI2c<2>();
-    sjsu::Mpu6050 rotunda(i2c, 0x66);
-    sjsu::Mpu6050 shoulder(i2c, 0x67);
+    uint8_t buff[1] = {1};
+    i2c.Initialize();
+    
+    // write to the base accelerometers to open the correct mux bus
+    sjsu::LogInfo("Initializing base accelerometers...");
+    i2c.Write(0x70, buff, 1);
+    sjsu::Mpu6050 rotunda(i2c, 0x68);
+    sjsu::Mpu6050 shoulder(i2c, 0x69);
+    InitializeBase(rotunda, shoulder);
+    
+    // write to the join accelerometers to open the correct mux bus
+    sjsu::LogInfo("Initializing joint accelerometers...");
+    sjsu::Delay(10ms);
+    buff[0] = static_cast<uint8_t>(buff[0] << 1);
+    i2c.Write(0x70, buff, 1);
     sjsu::Mpu6050 elbow(i2c, 0x68);
     sjsu::Mpu6050 wrist(i2c, 0x69);
-
-    sjsu::LogInfo("Initializing accelerometers...");
-    InitializeArm(rotunda, shoulder, elbow, wrist);
-
+    sjsu::LogInfo("done creating mpus");
+    InitializeJoints(elbow, wrist);
+    sjsu::Delay(10ms);
     while (true)
     {
+        sjsu::LogInfo("Starting main control loop");
+        buff[0] = 1;
+        i2c.Write(0x70, buff,1); // set [00000001]
         sjsu::LogInfo("Reading rotunda Mpu6050 acceleration data...");
         PrintAcceleration(rotunda);
+        sjsu::Delay(10ms);
         sjsu::LogInfo("Reading shoulder Mpu6050 acceleration data...");
         PrintAcceleration(shoulder);
+        sjsu::Delay(10ms);
+
+        buff[0] = static_cast<uint8_t>(buff[0] << 1);
+        i2c.Write(0x70, buff, 1); // set [00000010]
         sjsu::LogInfo("Reading elbow Mpu6050 acceleration data...");
         PrintAcceleration(elbow);
+        sjsu::Delay(10ms);
         sjsu::LogInfo("Reading wrist Mpu6050 acceleration data...");
         PrintAcceleration(wrist);
 
-        sjsu::Delay(100ms);
+        sjsu::Delay(10ms);
     }
     return 0;
 }
