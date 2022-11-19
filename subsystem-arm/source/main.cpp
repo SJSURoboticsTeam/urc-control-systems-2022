27050eed5cd3054169fc9be212f1be7b023b3394
Viha123 <sviha195@gmail.com>
2022-11-19 15:15:03 -0800

    rr9 works with sample mission control data in rr9-demo-branch

--- subsystem-arm/source/main.cpp
+++ subsystem-arm/source/main.cpp
@@ -45,9 +45,9 @@ int main()
   right_wrist_motor.settings.gear_ratio = 8;
 
   JointRouter joint_router(rotunda_motor, shoulder_motor, elbow_motor, left_wrist_motor, right_wrist_motor);
-  HandRouter hand_router(&pca9685);
+  // HandRouter hand_router(&pca9685);
   sjsu::Servo servo(sjsu::lpc40xx::GetPwm<1, 0>());
-  HandRouter rr9_claw_router(&servo);
+  HandRouter hand_router(&servo);
   MissionControlHandler mission_control;
   RulesEngine rules_engine;
   // TODO: Fix hand from crashing program when pca9685 is not connected!
@@ -69,10 +69,10 @@ int main()
       // TODO: Make this work w Joystick Serial
       printf("Received:\n%s\n", response.c_str());
       commands = mission_control.ParseMissionControlData(response);
-      commands = rules_engine.ValidateCommands(commands);
+      // commands = rules_engine.ValidateCommands(commands);
     }
     arguments = ModeSelect::SelectMode(commands, arguments);
-    commands.Print();
+    // commands.Print();
     if(commands.mode == 'J') {
       arguments.joint_args = joint_router.SetJointArguments(arguments.joint_args);
     }
