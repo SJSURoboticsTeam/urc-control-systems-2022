60a7acc096704dbb602dcd9f271bd70c4fb0323e
Viha123 <sviha195@gmail.com>
2022-11-19 14:35:46 -0800

    Hanyu, Nina, Viha added rr9 functionality to hand router

--- subsystem-arm/implementations/routers/hand-router.hpp
+++ subsystem-arm/implementations/routers/hand-router.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include "utility/math/units.hpp"
-
+#include "devices/actuators/servo/servo.hpp"
 #include "../pca9685.hpp"
 #include "../../dto/arm-dto.hpp"
 
@@ -9,12 +9,17 @@ namespace sjsu::arm
     class HandRouter
     {
     public:
-        HandRouter(sjsu::Pca9685 &pca_module) : pca_module_(pca_module){};
+        HandRouter(sjsu::Pca9685 *pca_module) : pca_module_(pca_module) {
+            pca_module_->ModuleInitialize();
+        }
 
-        // Initialize
-        void Initialize()
-        {
-            pca_module_.ModuleInitialize();
+        // rr9 servo ctor
+        HandRouter(sjsu::Servo *servo) : servo_(servo),  pca_module_(NULL){
+            servo_->settings.min_pulse = 500us;
+            servo_->settings.max_pulse = 2500us;
+            servo_->settings.min_angle = 0_deg;
+            servo_->settings.max_angle = 200_deg;
+            servo_->Initialize();
         }
 
         std::chrono::microseconds AngleToPulseWidth(int angle)
@@ -25,14 +30,15 @@ namespace sjsu::arm
         hand_arguments SetHandArguments(hand_arguments hand_arguments, char mode)
         {
             if(mode == 'H') {
-                pca_module_.setPulseWidth(0, AngleToPulseWidth(hand_arguments.pinky_angle));
-                pca_module_.setPulseWidth(1, AngleToPulseWidth(hand_arguments.ring_angle));
-                pca_module_.setPulseWidth(2, AngleToPulseWidth(hand_arguments.middle_angle));
-                pca_module_.setPulseWidth(3, AngleToPulseWidth(hand_arguments.index_angle));
-                pca_module_.setPulseWidth(4, AngleToPulseWidth(hand_arguments.thumb_angle));
+                pca_module_->setPulseWidth(0, AngleToPulseWidth(hand_arguments.pinky_angle));
+                pca_module_->setPulseWidth(1, AngleToPulseWidth(hand_arguments.ring_angle));
+                pca_module_->setPulseWidth(2, AngleToPulseWidth(hand_arguments.middle_angle));
+                pca_module_->setPulseWidth(3, AngleToPulseWidth(hand_arguments.index_angle));
+                pca_module_->setPulseWidth(4, AngleToPulseWidth(hand_arguments.thumb_angle));
             }
             else if(mode == 'R') {
                 // do logic for RR9 moving here
+                servo_->SetAngle(units::angle::degree_t(hand_arguments.pinky_angle));
             }
             return hand_arguments;
         }
@@ -42,6 +48,8 @@ namespace sjsu::arm
     private:
         // sjsu::arm::arm_arguments arguments_;
         // hand_arguments hand_arguments_;
-        sjsu::Pca9685 &pca_module_;
+        sjsu::Pca9685 *pca_module_ = NULL;
+        sjsu::Servo *servo_ = NULL;
+        char mode_;
     };
 }
