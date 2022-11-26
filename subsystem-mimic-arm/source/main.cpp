311fe83426fb3da2944cf062f7522aa9b89f4185
PhazonicRidley <ma13hew@gmail.com>
2022-11-25 19:41:00 -0800

    Processed voltage into angles and set zeros for each angle

--- subsystem-mimic-arm/source/main.cpp
+++ subsystem-mimic-arm/source/main.cpp
@@ -22,6 +22,21 @@ float VoltageToTrueDegree(float voltage, float max_voltage, float max_degree)
     return sjsu::Map(voltage, 0, max_voltage, 0, max_degree);
 }
 
+/// @brief Changes where zero degrees is. Processes a true degree to be in relation to the offset degree. 
+/// Example: If the new zero in true degrees is 90 degrees, and you pass in an input of 45 degrees, you're result will be -45 degrees.
+/// @param input_deg The angle in true degrees you wish to map.
+/// @param new_zero The new location on the unit circle where zero should start.
+/// @return The phase shifted angle.
+float DegreePhaseShift(float input_deg, float new_zero)
+{
+    if (new_zero < 0 || new_zero > 360)
+        throw std::invalid_argument("This function only considers the unit circle. Please set new_zero to the position on the unit circle it will be.");
+    
+    while (input_deg > 360 || input_deg < -360)
+        input_deg = input_deg / 360;
+    
+    return input_deg - new_zero;
+}
 
 int main()
 {
@@ -35,6 +50,8 @@ int main()
     sjsu::LogInfo("Signal pins configured");
     sjsu::LogInfo("ADC initialized.");
     
+    // demo code
+    // TODO: move to its own file.
     while(true)
     {
         const size_t N = 3;
@@ -42,9 +59,27 @@ int main()
         std::array<float, N> output = digital_multiplexer.ReadAll<N>(channels);
         for (int i = 0; i < N; i++)
         {
-            sjsu::LogInfo("Channel %i: Degree: %f", channels[i], VoltageToTrueDegree(output[i], 3.3, 180));
+            float degree = DegreePhaseShift(VoltageToTrueDegree(output[i], 3.3, 180), 90);
+            sjsu::LogInfo("Channel %i: Degree: %f", channels[i], degree);
            // sjsu::LogInfo("Channel %i: Voltage: %f", channels[i], output[i]);
         }
+    }
+
+    // Control loop for mimic arm controller
+    // Rotunda (channel 0) and wrist rotate (channel 4)
+    while (true)
+    {
+        // read voltages from pots
+        std::array<float, 5> output_voltages = digital_multiplexer.ReadAll<5>({0, 1, 2, 3, 4});
+        std::array<float, 5> degree_conversion = {360, 90, 90, 90, 360};
+        std::array<float, 5> results = {};
+        for (int i = 0; i < 5; i++)
+        {
+            float true_degree = VoltageToTrueDegree(output_voltages[i], 3.3, 360);
+            results[i] = DegreePhaseShift(true_degree, degree_conversion[i]);
+        }
+
+        // TODO: POST to mission control various degrees
     }
 
     return 0;
