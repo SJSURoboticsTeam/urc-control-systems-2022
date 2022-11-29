5ca6025bf7bd4c3776d2ff6c6036886d74936524
Alexander Pereira <31743078+50xp50@users.noreply.github.com>
2022-11-29 13:43:41 -0800

    - Updated rules engine to include clamping for joint mode and to take into account mc_commands  mode (#207)
    - Fixed arm mode test to work with new arm arguments format
    - Fixed hand mode test to work with new arm arguments format
    - Fixed one-liner merge conflict that was in mission control handler test
    - Fixed rules engine test cases to work with new arm arguments format and mc_commands mode
    
    Co-authored-by: Coreyboy1820 <92711317+Coreyboy1820@users.noreply.github.com>

--- subsystem-arm/test/arm-modes_test.cpp
+++ subsystem-arm/test/arm-modes_test.cpp
@@ -7,26 +7,16 @@ namespace sjsu::arm
 {
     TEST_CASE("Arm Mode Testing")
     {
-        SECTION("should set angles to correct simultaneous mode angles")
+        SECTION("should set joint angles based off mc commands")
         {
-            joint_arguments original_commands = {10, 20, 30, 40, 50, 60, 70, 80};
-            joint_arguments args = JointModes::SimultaneousMode(original_commands);
-            CHECK(args.rotunda_angle == original_commands.rotunda_angle);
-            CHECK(args.shoulder_angle == original_commands.shoulder_angle);
-            CHECK(args.elbow_angle == original_commands.elbow_angle);
-            CHECK(args.wrist_pitch_angle == original_commands.wrist_pitch_angle);
-            CHECK(args.wrist_roll_angle == original_commands.wrist_roll_angle);
-        }
-
-        SECTION("should ignore arguments and set angles to proper condensed mode angles")
-        {
-            joint_arguments original_commands = {10, 10, 10, 10, 10, 10, 10, 10, 'S'};
-            joint_arguments args = JointModes::CondensedMode(original_commands);
-            CHECK_NE(args.rotunda_angle, original_commands.rotunda_angle);
-            CHECK_NE(args.shoulder_angle, original_commands.shoulder_angle);
-            CHECK_NE(args.elbow_angle, original_commands.elbow_angle);
-            CHECK_NE(args.wrist_pitch_angle, original_commands.wrist_pitch_angle);
-            CHECK_NE(args.wrist_roll_angle, original_commands.wrist_pitch_angle);
+            mc_commands commands = {first_angle:1, second_angle:2, third_angle:3, fourth_angle:4, fifth_angle:5};
+            arm_arguments args;
+            args = ArmMode::JointMode(commands, args);
+            CHECK(args.joint_args.rotunda_angle == commands.first_angle);
+            CHECK(args.joint_args.shoulder_angle == commands.second_angle);
+            CHECK(args.joint_args.elbow_angle == commands.third_angle);
+            CHECK(args.joint_args.wrist_pitch_angle == commands.fourth_angle);
+            CHECK(args.joint_args.wrist_roll_angle == commands.fifth_angle);
         }
     }
 }
